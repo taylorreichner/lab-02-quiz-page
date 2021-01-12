4bf4b4b25b159631f18e29c1e7eacb35837abd72
taylorreichner <taylorreichner@yahoo.com>
2021-01-11 16:32:52 -0800

    added result function to java script

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
 // import functions and grab DOM elements
 
+import { countsAsAYes } from "./ultis.js";
+
+
+const quizButton = document.getElementById('quiz-button');
+const answerArea = document.getElementById('answer-area');
+
+quizButton.addEventListener('click', () => {
+    alert('Hello! This will not be easy');
+    
+const confirmation = confirm('Want to take my test?')
+
+if (!confirmation) {
+    return;
+}
+ 
+const firstName = prompt('what is your first name?');
+const lastName = prompt('what is your last name?');
+const firstAnswer = prompt('Are there roughly 60 cat breeds?');
+
+let correctAnswers = 0;
+
+if (countsAsAYes(firstAnswer)) {
+    correctAnswers++;
+} else {
+
+}
+const secondAnswer = prompt('Is a green zebra a type of cat?');
+
+if (!countsAsAYes(secondAnswer)) {
+    correctAnswers++;
+} else {
+
+}
+
+const resultsString = 'you got ' + correctAnswers + ' correct!';
+
+answerArea.textContent = resultsString;
+
+});
+
+
 // initialize state
 
 // set event listeners to update state and DOM
